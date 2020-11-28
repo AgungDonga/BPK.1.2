327600f385cd341f59c104fe2be134d7520dccd7
Bagus WIdyantoro Putro <baguswidyantoroputro@gmail.com>
2020-11-28 14:30:49 +0700

    Perbaikan detail penagihan

--- src/main/java/com/example/buanaMekar/repositories/SuratJalanRepository.java
+++ src/main/java/com/example/buanaMekar/repositories/SuratJalanRepository.java
@@ -35,7 +35,7 @@ public interface SuratJalanRepository extends CrudRepository<SuratJalan, Integer
     @Query("SELECT sj, o FROM SuratJalan sj, Orderan o WHERE sj.status = 1 AND sj.tglKirim= ?2 AND o.toko.namaToko = ?1 AND o.id = sj.orderan")
     List<SuratJalan> listDetailInvoice(String key1, String key2);
     
-    @Query("SELECT sj, o FROM SuratJalan sj, Orderan o , Invoice i, Penagihan p WHERE i.status = 1 AND sj.tglKirim= ?2 AND o.toko.namaToko = ?1 AND o.id = sj.orderan AND i.invoice = p.invoice")
+    @Query("SELECT DISTINCT sj, o FROM SuratJalan sj, Orderan o , Invoice i, Penagihan p WHERE i.status = 1 AND sj.tglKirim= ?2 AND o.toko.namaToko = ?1 AND o.id = sj.orderan AND i.invoice = p.invoice GROUP BY i.invoice")
     List<SuratJalan> listDetailPenagihan(String key1, String key2);
     
 //    @Query(nativeQuery = true,value = "call getAllPesanan(:namaToko, :tanggalL)")   // call store procedure with arguments
